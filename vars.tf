72757fef628e331dc3c98dd98c9b76c95a4e085c
nickolaskwiecien <kwiecinj@alfredstate.edu>
2020-01-10 12:23:46 -0500

    Delete vars.tf

--- vars.tf
+++
@@ -1,7 +0,0 @@
-variable "region" {
-  description = "The region you would like to deploy in. Default us-east-1"
-}
-variable "iam_profile" {
-  description = "the iam profile asssociated with the account you would like to deploy in"
-  default = ["us-east-1"]
-}
